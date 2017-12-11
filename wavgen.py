7bc034b7b227bff29b70d4d46f5ac2d857d01273
frank-deng <13382084679@163.com>
2017-12-11 23:26:01 +0800

    Updatef

--- wavgen.py
+++ wavgen.py
@@ -5,6 +5,6 @@ with wave.open('1.wav', 'wb') as f:
     f.setnchannels(1);
     f.setframerate(44100);
     f.setsampwidth(2);
-    wavdata = [math.sin(n/180*math.PI) for n in range(88888)];
+    wavdata = [0.5*math.sin(n/20*2*math.pi+0.6*math.pi)+0.5* math.sin(n/44.1*2*math.pi) for n in range(200000)];
     wavdata = b''.join([struct.pack('h', int(sample*32000)) for sample in wavdata]);
     f.writeframes(wavdata);
