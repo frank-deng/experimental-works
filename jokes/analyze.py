ecd2d42d40f440893b7f378fca0d252ae0d5327d
frank-deng <13382084679@163.com>
2017-12-22 10:05:56 +0800

    Updated

--- jokes/analyze.py
+++ jokes/analyze.py
@@ -44,63 +44,67 @@ with open('uselessWords.txt', 'r') as f:
     while word:
         uselessWords[word] = True;
         word = f.read();
-    
-rejectedCount = 0;
-acceptedCount = 0;
-wordsBlocked = {}
+
 wordsAccepted = {}
+wordsBlocked = {}
+wa = {}
+wb = {}
 
 for joke in jokes:
-    added = False;
-    if 0 == joke['accept']:
+    wa.clear();
+    wb.clear();
+    if 1 == joke['accept']:
         for word in jieba.cut(joke['text']):
-            if not wordsBlocked.get(word):
-                rejectedCount += 1;
-                wordsBlocked[word] = 1;
-            elif not added:
-                rejectedCount += 1;
-                wordsBlocked[word] += 1;
-                added = True;
-    elif 1 == joke['accept']:
+            if word in uselessWords:
+                continue;
+            wa[word] = wa.get(word, 1);
+    elif 0 == joke['accept']:
         for word in jieba.cut(joke['text']):
-            if not wordsAccepted.get(word):
-                acceptedCount += 1;
-                wordsAccepted[word] = 1;
-            elif not added:
-                acceptedCount += 1;
-                wordsAccepted[word] += 1;
-                added = True;
-
-for w in copy.deepcopy(wordsBlocked):
-    if uselessWords.get(w):
-        del wordsBlocked[w];
+            if word in uselessWords:
+                continue;
+            wb[word] = wb.get(word, 1);
+    for w in wa:
+        wordsAccepted[w] = wordsAccepted.get(w, 0) + wa[w];
+    for w in wb:
+        wordsBlocked[w] = wordsBlocked.get(w, 0) + wb[w];
+        
+rejectedCount = 0;
+for w in wordsBlocked:
+	rejectedCount += wordsBlocked[w];
+acceptedCount = 0;
+for w in wordsAccepted:
+	acceptedCount += wordsAccepted[w];
         
 jokes2 = [
-'''
-妹子站在小通道哪里，我刚好要从那过，就和她说：麻烦，借过。结果妹子说：借过可以，什么时候还？？？
-''',
-'''
-课堂上老师点名：“刘华！”\n结果下面一孩子大声回到：“yeah！”\n老师很生气：“为什么不说‘到’？”\n孩子说：“那个字念‘烨’……”
-''',
+{'text':'妹子站在小通道哪里，我刚好要从那过，就和她说：麻烦，借过。结果妹子说：借过可以，什么时候还？？？'},
+{'text':'课堂上老师点名：“刘华！”\n结果下面一孩子大声回到：“yeah！”\n老师很生气：“为什么不说‘到’？”\n孩子说：“那个字念‘烨’……”'},
 ]
 
 #for item in models.getJokes(1, 10):
-#    joke = item['text'];
 for item in jokes2:
-    joke = item;
+    joke = item['text'];
+    words = {};
     for word in jieba.cut(joke):
-        wb = 0;
-        if wordsBlocked.get(word):
-            wb = wordsBlocked[word];
-        wa = 0;
-        if wordsAccepted.get(word):
-            wa = wordsAccepted[word];
-        if wb == 0:
+        words[word] = True;
+        
+    pa, pb = 0, 0;
+    for word in words:
+        wa = wordsAccepted.get(word, 0);
+        wb = wordsBlocked.get(word, 0);
+        if wa == 0 and wb == 0:
             continue;
-        p0 = (wb / rejectedCount);
-        p1 = rejectedCount / (rejectedCount + acceptedCount);
-        p2 = (wa + wb) / (rejectedCount + acceptedCount);
-        print(word, p0 * p1 / p2);
+        pa1 = (wa + 1) / (acceptedCount + 2);
+        pb1 = (wb + 1) / (rejectedCount + 2);
+        if pa == 0:
+            pa = pa1;
+        else:
+            pa *= pa1;
+        if pb == 0:
+            pb = pb1;
+        else:
+            pb *= pb1;
+        print(pa1, pb1);
+    print(pa, pb, pa > pb);
     print(joke);
     print('');
 
