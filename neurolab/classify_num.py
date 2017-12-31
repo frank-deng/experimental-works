e53189c5bc2228c73ad3688f585f0d877d52afa1
frank-deng <13382084679@163.com>
2017-12-31 15:47:57 +0800

    classify_num.py updated

--- neurolab/classify_num.py
+++ neurolab/classify_num.py
@@ -19,20 +19,28 @@ def getTrainData(cnt):
     trainData = [];
     trainResult = [];
     for i in range(cnt):
-        n = randint(-32768, 32767);
+        n = randint(-2147483648, 2147483647);
         trainData.append([int(b) for b in num2bin(n)]);
         trainResult.append([int(b) for b in classifyNum(n)]);
     return trainData, trainResult;
 
-trainData, trainResult = getTrainData(100);
+trainData, trainResult = getTrainData(1000);
 trainData = np.array(trainData);
 trainResult = np.array(trainResult);
 
-transFunc = nl.trans.LogSig();
-net = nl.net.newff([[0, 1]] * 32, [32, 10, 4], [transFunc, transFunc, transFunc]);
+net = nl.net.newff([[0, 1]] * 32, [32, 10, 4], [nl.trans.LogSig(), nl.trans.LogSig(), nl.trans.LogSig()]);
 err = net.train(trainData, trainResult, epochs=1000, show=1, goal=0.0001)[-1];
 
-for i in range(10):
-    n = randint(-32768, 32767);
-    print(num2bin(n), classifyNum(n), net.sim([[int(b) for b in num2bin(n)]]));
+total = 10000;
+good, bad = 0, 0
+for i in range(total):
+    n = randint(-2147483648, 2147483647);
+    resSim = net.sim([[int(b) for b in num2bin(n)]])[0];
+    res = ''.join([str(int(a > 0.5)) for a in resSim]);
+    if res != classifyNum(n):
+        bad += 1;
+    else:
+        good += 1;
+
+print('Total: %d\nCorrect: %d\nIncorrect: %d'%(total, good, bad));
 
