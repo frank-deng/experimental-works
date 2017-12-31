8862ad2abe0157d02338301c6e8f612a89d5a4e7
frank-deng <13382084679@163.com>
2017-12-31 12:50:55 +0800

    Updated

--- neurolab/xor.py
+++ neurolab/xor.py
@@ -10,11 +10,10 @@ trainData = np.array([
     (1, 1),
 ]);
 trainResult = np.array([[a ^ b] for a, b in trainData]);
-transFunc = nl.trans.TanSig();
-net = nl.net.newff([[0, 1], [0, 1]], [2, 1], [transFunc, transFunc]);
+transFunc = nl.trans.LogSig();
+net = nl.net.newff([[0, 1], [0, 1]], [2, 2, 1], [transFunc, transFunc, transFunc]);
 err = net.train(trainData, trainResult, epochs=1000, show=1, goal=0.0001)[-1];
 
 for a, b in trainData:
     print(net.sim([[a, b]]));
 
-print(trainResult);
