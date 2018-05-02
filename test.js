52fa2cc996361481a5a58cc9c04e7d2085bdba57
frank-deng <13382084679@163.com>
2018-05-02 20:38:07 +0800

    Updated

--- test.js
+++ test.js
@@ -8,3 +8,18 @@ var a = new myObj('hahaha');
 var b = new myObj('Frank!!!');
 console.log(a.getVal());
 console.log(b.getVal());
+
+function abc(){
+	try{
+		jskhdb();
+		return '$hahaha';
+	} catch(e) {
+		console.log('oops');
+		return '$oops';
+	} finally {
+		console.log('finished');
+		//return '$finished';
+	}
+	return 1;
+}
+console.log(abc());
