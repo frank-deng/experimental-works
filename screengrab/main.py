e6a6b531fce168d55e43b7ee8d32e8265671b33a
frank-deng <13382084679@163.com>
2018-05-04 23:40:29 +0800

    BLEllena add auto keyboard operation

--- screengrab/main.py
+++ screengrab/main.py
@@ -36,26 +36,54 @@ def printMoves(moves):
     elif (len(moves) == 1):
         print(labelMoves[moves[0]]);
 
+from pykeyboard import PyKeyboard;
+def performMove(move):
+    k = PyKeyboard();
+    keyOper = (k.up_key, k.left_key, k.right_key, k.down_key);
+    try:
+        if (len(move) > 1):
+            k.press_key(k.space);
+            time.sleep(0.5);
+            k.release_key(k.space);
+        else:
+            k.press_key(keyOper[move[0]]);
+            time.sleep(0.5);
+            k.release_key(keyOper[move[0]]);
+    except KeyError:
+        print('Unknown move: %s'%move);
+
 ellena = BLEllena();
 try:
     lastMove = [];
     lastStatus = None;
+    moves = [];
+    timestamp = time.time();
     while True:
         status = ellena.getStatus();
         if (None != status and status != lastStatus):
             lastStatus = status;
             if BLEllena.ELLENA_WATCHING == status:
                 print('\nWatching:');
+                timestamp = time.time();
+                moves = [];
             elif BLEllena.ELLENA_ACTIVE == status:
                 print('Operating...');
+                timestamp = time.time();
 
         if (BLEllena.ELLENA_WATCHING == status):
             move = ellena.getMove();
             if (len(move) and (set(lastMove) != set(move))):
                 printMoves(move);
                 lastMove = move;
-        else:
+                moves.append({
+                    'ts': time.time() - timestamp,
+                    'mv': move,
+                });
+        elif (BLEllena.ELLENA_ACTIVE == status):
             lastMove = [];
+            if (len(moves) > 0 and time.time() - timestamp >= moves[0]['ts']):
+                performMove(moves[0]['mv']);
+                moves.pop(0);
         time.sleep(0.05);
 except KeyboardInterrupt:
     pass;
