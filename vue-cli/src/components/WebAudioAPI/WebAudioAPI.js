981213ca5d308f20fbde2ba53b19d11fe29518eb
frank-deng <13382084679@163.com>
2018-06-09 23:45:53 +0800

    vue-cli updated webaudioapi

--- vue-cli/src/components/WebAudioAPI/WebAudioAPI.js
+++ vue-cli/src/components/WebAudioAPI/WebAudioAPI.js
@@ -2,26 +2,41 @@ export default {
 	data(){
 		return {
 			audioCtx: undefined,
+			gainNode: undefined,
 			oscillator: undefined,
+			soundType: 'sine',
+			running: false,
+			frequency: 1000,
+			volume: 0.5,
 		};
 	},
 	methods: {
+		changeVolume(vol){
+			if (this.running) {
+				this.gainNode.gain.setValueAtTime(vol, this.audioCtx.currentTime);
+			}
+		},
 		toggleBeep(){
-			if (this.oscillator){
+			if (this.running){
 				this.oscillator.stop();
-				this.oscillator = undefined;
+				this.running = false;
 			} else {
 				let oscillator = this.audioCtx.createOscillator();
-				oscillator.type = 'sine';
-				oscillator.frequency.setValueAtTime(1000, this.audioCtx.currentTime);
-				oscillator.connect(this.audioCtx.destination);
+				oscillator.type = this.soundType;
+				oscillator.frequency.setValueAtTime(this.frequency, this.audioCtx.currentTime);
+				oscillator.connect(this.gainNode);
 				oscillator.start();
 				this.oscillator = oscillator;
+				this.running = true;
 			}
 		},
 	},
 	mounted(){
 		this.audioCtx = new AudioContext();
+		let gainNode = this.audioCtx.createGain();
+		gainNode.connect(this.audioCtx.destination);
+		gainNode.gain.setValueAtTime(this.volume, this.audioCtx.currentTime);
+		this.gainNode = gainNode;
 	},
 }
 
--- vue-cli/src/components/WebAudioAPI/WebAudioAPI.vue
+++ vue-cli/src/components/WebAudioAPI/WebAudioAPI.vue
@@ -1,6 +1,38 @@
 <template>
 	<div>
-		<el-button @click='toggleBeep'>Beep</el-button>
+		<el-row>
+			<el-col :span='24'>
+				<p class='itemTitle'>类型</p>
+				<el-select v-model="soundType" :disabled='running'>
+					<el-option label='正弦波' value='sine'></el-option>
+					<el-option label='方波' value='square'></el-option>
+					<el-option label='三角波' value='triangle'></el-option>
+					<el-option label='锯齿波' value='sawtooth'></el-option>
+				</el-select>
+			</el-col>
+			<el-col :span='24'>
+				<p class='itemTitle'>频率</p>
+				<el-input-number v-model="frequency" :min="1" :max="44100" :disabled='running'></el-input-number>
+			</el-col>
+			<el-col :span='24'>
+				<p class='itemTitle'>音量</p>
+				<el-slider v-model="volume" :min='0' :max='1' :step='0.001' :show-tooltip='false' @change='changeVolume'></el-slider>
+			</el-col>
+			<el-col :span='24'><el-button type='primary' @click='toggleBeep'>{{running ? '停止' : '启动'}}</el-button></el-col>
+		</el-row>
 	</div>
 </template>
 <script src='./WebAudioAPI.js'></script>
+<style scoped>
+.itemTitle{
+	display: block;
+	margin: 2px 0;
+	padding: 0;
+}
+.el-slider{
+	margin: 0 12px;
+}
+.el-col {
+	margin: 6px 0;
+}
+</style>
