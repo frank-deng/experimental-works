06f856897955f84589902710c6d78d779dd7cc04
frank-deng <13382084679@163.com>
2018-06-09 22:35:54 +0800

    vue-cli add mobile-logger.js

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -15,11 +15,35 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  overflow: auto;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 30%;
+  padding: 8px;
 }
 ul, li{
 	margin: 0;
 	padding: 0;
 }
+#logger_element{
+	display:block;
+	position:fixed;
+	left:0;
+	right:0;
+	bottom:0;
+	height:30%;
+	font-size:1em;
+	line-height:1.1em;
+	font-family:monospace;
+	font-weight:normal;
+	color:#000;
+	background-color:#ffd7dd;
+	margin:0;
+	padding:1px 0 0 0;
+	z-index: 100;
+}
 </style>
+
