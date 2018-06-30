90e79a45cd352b7049904c7bc641e97d6a26967a
frank-deng <13382084679@163.com>
2018-06-30 22:42:48 +0800

    Add DTMF sound

--- vue-cli/src/components/DTMFEffect/index.vue
+++ vue-cli/src/components/DTMFEffect/index.vue
@@ -2,45 +2,57 @@
 	<div>
 		<el-row class='dialPad' :gutter='2'>
 			<el-col	:span='8'>
-				<el-button type='primary'>7</el-button>
+				<div class='dialBtn' @mousedown='dial("7")' @touchstart='dial("7")' @mouseup='dial()' @touchend='dial()'>7</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>8</el-button>
+				<div class='dialBtn' @mousedown='dial("8")' @touchstart='dial("8")' @mouseup='dial()' @touchend='dial()'>8</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>9</el-button>
+				<div class='dialBtn' @mousedown='dial("9")' @touchstart='dial("9")' @mouseup='dial()' @touchend='dial()'>9</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>4</el-button>
+				<div class='dialBtn' @mousedown='dial("4")' @touchstart='dial("4")' @mouseup='dial()' @touchend='dial()'>4</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>5</el-button>
+				<div class='dialBtn' @mousedown='dial("5")' @touchstart='dial("5")' @mouseup='dial()' @touchend='dial()'>5</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>6</el-button>
+				<div class='dialBtn' @mousedown='dial("6")' @touchstart='dial("6")' @mouseup='dial()' @touchend='dial()'>6</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>1</el-button>
+				<div class='dialBtn' @mousedown='dial("1")' @touchstart='dial("1")' @mouseup='dial()' @touchend='dial()'>1</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>2</el-button>
+				<div class='dialBtn' @mousedown='dial("2")' @touchstart='dial("2")' @mouseup='dial()' @touchend='dial()'>2</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>3</el-button>
+				<div class='dialBtn' @mousedown='dial("3")' @touchstart='dial("3")' @mouseup='dial()' @touchend='dial()'>3</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>*</el-button>
+				<div class='dialBtn' @mousedown='dial("*")' @touchstart='dial("*")' @mouseup='dial()' @touchend='dial()'>*</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>0</el-button>
+				<div class='dialBtn' @mousedown='dial("0")' @touchstart='dial("0")' @mouseup='dial()' @touchend='dial()'>0</div>
 			</el-col>
 			<el-col	:span='8'>
-				<el-button type='primary'>#</el-button>
+				<div class='dialBtn' @mousedown='dial("#")' @touchstart='dial("#")' @mouseup='dial()' @touchend='dial()'>#</div>
 			</el-col>
 		</el-row>
 	</div>
 </template>
 <style scoped>
+.dialBtn {
+	display: block;
+	width: 100%;
+	line-height: 80px;
+	background-color: rgb(64,158,255);
+	color: #ffffff;
+	text-align: center;
+	font-size: 24px;
+	border-radius: 4px;
+	cursor: pointer;
+	user-select: none;
+}
 .dialPad {
 	position: absolute;
 	bottom: 0;
@@ -49,11 +61,6 @@
 }
 .el-col {
 	margin-bottom: 2px;
-}
-.el-button{
-	display: block;
-	width: 100%;
-	height: 80px;
 }
 </style>
 <script src="./main.js"></script>
--- vue-cli/src/components/DTMFEffect/main.js
+++ vue-cli/src/components/DTMFEffect/main.js
@@ -1,4 +1,19 @@
 export default{
+	data(){
+		return {
+			dialNum: undefined,
+		};
+	},
+	watch: {
+		'dialNum'(dialNum){
+			console.log(dialNum);
+		},
+	},
+	methods: {
+		dial(number){
+			this.dialNum = number;
+		},
+	},
 	mounted(){
 	},
 }
