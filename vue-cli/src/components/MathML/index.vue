a3d09541f0d49356318d89d0b9baea2e8bd09df9
frank-deng <13382084679@163.com>
2018-07-05 23:07:01 +0800

    Add MathJax support

--- vue-cli/src/components/MathML/index.vue
+++ vue-cli/src/components/MathML/index.vue
@@ -1,4 +1,18 @@
 <template>
+	<math xmlns="http://www.w3.org/1998/Math/MathML">
+         <mrow>
+			<msup><mi>a</mi><mn>2</mn></msup>
+			<mo>+</mo>
+			<msup><mi>b</mi><mn>2</mn></msup>
+			<mo>=</mo>
+			<msup><mi>c</mi><mn>2</mn></msup>
+         </mrow>
+	</math>
 </template>
 <script type="text/x-mathjax-config">
+export default {
+	mounted(){
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	},
+}
 </script>
