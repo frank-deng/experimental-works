b3669a394accaefd796cffbe6027d5548fe7f29e
frank-deng <13382084679@163.com>
2018-07-26 23:38:59 +0800

    Updated

--- vue-cli/src/router/index.js
+++ vue-cli/src/router/index.js
@@ -4,9 +4,8 @@ import Router from 'vue-router'
 import IndexPage from '@/components/IndexPage/IndexPage.vue'
 import DTMFEffect from '@/components/DTMFEffect/index.vue'
 import WebAudioAPI from '@/components/WebAudioAPI/WebAudioAPI.vue'
-import ColorDist from '@/components/ColorDist/index.vue'
-import WebGLTry from '@/components/WebGLTry/index.vue'
 import MathML from '@/components/MathML/index.vue'
+//import WebGLTry from '@/components/WebGLTry/index.vue'
 
 Vue.use(Router);
 let router = new Router({
@@ -22,25 +21,22 @@ let router = new Router({
 			component: DTMFEffect,
 		},
 		{
-			path: '/colordist',
-			name: '颜色分布',
-			component: ColorDist,
-		},
-		{
 			path: '/WebAudioAPI',
 			name: 'WebAudioAPI实验',
 			component: WebAudioAPI,
-		},
-		{
-			path: '/WebGL',
-			name: 'WebGL/three.js测试',
-			component: WebGLTry,
 		},
 		{
 			path: '/MathML',
 			name: 'MathML测试',
 			component: MathML,
 		},
+		/*
+		{
+			path: '/WebGL',
+			name: 'WebGL/three.js测试',
+			component: WebGLTry,
+		},
+		*/
 	],
 });
 router.beforeEach((to, from, next)=>{
@@ -48,3 +44,4 @@ router.beforeEach((to, from, next)=>{
 	next();
 });
 export default router;
+
