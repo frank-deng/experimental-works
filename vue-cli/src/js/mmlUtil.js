d37fda9ca3a84ae8983a66bdd41b9cb80b969b02
frank-deng <13382084679@163.com>
2018-08-26 10:57:43 +0800

    Updated

--- vue-cli/src/js/mmlUtil.js
+++ vue-cli/src/js/mmlUtil.js
@@ -1,9 +1,22 @@
-export function processMML(_mml) {
+'use strict';
+var noteFreq = [
+	16.352, 17.324, 18.354, 19.445, 20.602, 21.827, 23.125, 24.500, 25.967, 27.500, 29.135, 30.868,
+	32.703, 34.648, 36.709, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55.000, 58.270, 61.736,
+	65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.83, 110.0,  116.54, 123.47,
+	130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.0,  233.08, 246.94,
+	261.63, 227.18, 293.66, 311.13, 326.63, 349.23, 369.99, 392.00, 415.30, 440.0,  466.16, 493.88,
+	523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880.0,  932.33, 987.77,
+	1046.5, 1108.7, 1174.7, 1244.5, 1318.5, 1396.9, 1480.0, 1568.0, 1661.2, 1760.0, 1864.7, 1975.53,
+	2093.0, 2217.5, 2349.3, 2489.0, 2637.0, 2793.8, 2960.0, 3136.0, 3322.4, 3520.0, 3729.3, 3951.05,
+	4186.0, 4439.9, 4498.6, 4978.0, 5474.0, 5587.7, 5919.9, 6271.9, 6644.9, 7040.0, 7458.6, 7902.1,
+];
+export function processMML(_mml){
 	//Parse mml into notes
 	var mml = _mml.slice(), match = undefined, pos = 0, commands = [];
-	var validNotes = {
-		'C':true, 'C+':true, 'D':true, 'D+':true, 'E':true, 'F':true,
-		'F+':true, 'G':true, 'G+':true, 'A':true, 'A+':true, 'B':true,
+	var note2num = {
+		'P': 0,
+		'C':1, 'C+':2, 'D':3, 'D+':4, 'E':5, 'F':6,
+		'F+':7, 'G':8, 'G+':9, 'A':10, 'A+':11, 'B':12,
 	};
 	var noteConv = {
 		'D-': 'C+',
@@ -15,19 +28,19 @@ export function processMML(_mml) {
 	while (mml.length) {
 		if (match = /^\s+/.exec(mml)) {
 			//Ignore white space chars
-		} else if (match = /^(T|L|O|P)(\d+)/i.exec(mml)) {
+		} else if (match = /^(T|L|O)(\d+)/i.exec(mml)) {
 			//Set tempo, note length, octave, pause
 			commands.push([match[1].toUpperCase(), Number(match[2])]);
 		} else if (match = /^(ML|MN|MS|\<|\>)/i.exec(mml)) {
 			//Set music legato, normal, staccato, control octaves
 			commands.push([match[0].toUpperCase()]);
-		} else if (match = /^([CDEFGAB][#+-]?)(\d*)(\.*)/i.exec(mml)) {
+		} else if (match = /^([CDEFGAB][#+-]?|P)(\d*)(\.*)/i.exec(mml)) {
 			//Process note
 			let note = match[1].toUpperCase().replace('#', '+');
 			if (noteConv[note]) {
 				note = noteConv[note];
 			}
-			if (!validNotes[note]) {
+			if (undefined === note2num[note]) {
 				throw SyntaxError('Invalid note ' + match[1] + ' at position ' + pos);
 			}
 			commands.push([note, Number(match[2]), match[3]]);
@@ -41,17 +54,6 @@ export function processMML(_mml) {
 	//Parse notes to instructions
 	var octave = 5, baseDuration = 60 * 4 / 128, noteLenDef = 4, gap = 0.01, time = 0;
 	var result = [];
-	var noteFreq = [
-		16.352, 17.324, 18.354, 19.445, 20.602, 21.827, 23.125, 24.500, 25.967, 27.500, 29.135, 30.868,
-		32.703, 34.648, 36.709, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55.000, 58.270, 61.736,
-		65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.83,	110.0, 	116.54,	123.47,
-		130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.0,  233.08, 246.94,
-		261.63, 227.18, 293.66, 311.13, 326.63, 349.23, 369.99, 392.00, 415.30, 440.0,  466.16, 493.88,
-		523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880.0,  932.33, 987.77,
-		1046.5, 1108.7, 1174.7, 1244.5, 1318.5, 1396.9, 1480.0, 1568.0, 1661.2, 1760.0, 1864.7, 1975.53,
-		2093.0, 2217.5, 2349.3, 2489.0, 2637.0, 2793.8, 2960.0, 3136.0, 3322.4, 3520.0, 3729.3, 3951.05,
-		4186.0, 4439.9, 4498.6, 4978.0, 5474.0, 5587.7, 5919.9, 6271.9, 6644.9, 7040.0, 7458.6, 7902.1,
-	];
 	var commandProcessor = {
 		'ML':()=>{
 			gap = 0;
@@ -60,7 +62,7 @@ export function processMML(_mml) {
 			gap = 0.1;
 		},
 		'MN':()=>{
-			gap = 0.05;
+			gap = 0.01;
 		},
 		'<':()=>{
 			if (octave > 0){
@@ -79,13 +81,11 @@ export function processMML(_mml) {
 			noteLenDef = len;
 		},
 		'O':(_octave)=>{
-			octave = _octave;
+			octave = _octave+1;
+			if (octave >= 8){
+				octave = 8;
+			}
 		},
-	};
-	var note2num = {
-		'P': 0,
-		'C':1, 'C+':2, 'D':3, 'D+':4, 'E':5, 'F':6,
-		'F+':7, 'G':8, 'G+':9, 'A':10, 'A+':11, 'B':12,
 	};
 	for (let cmd of commands) {
 		if (commandProcessor[cmd[0]]) {
@@ -97,20 +97,38 @@ export function processMML(_mml) {
 			continue;
 		}
 		let duration = baseDuration / (cmd[1] || noteLenDef);
-		if (cmd[2]) {
-			duration *= Math.pow(1.5, cmd[2].length);
+		switch (cmd[2]) {
+			case '.':
+				duration *= 1.5;
+			break;
+			case '..':
+				duration *= 1.75;
+			break;
 		}
-		result.push({
-			freq: note2num[note]? noteFreq[note2num[note]-1+octave * 12] : 0,
-			time: time,
-		});
-		if (gap){
+
+		let freq = note2num[note]? noteFreq[note2num[note]-1+octave * 12] : 0;
+
+		if (!gap){
+			result.push({
+				freq: freq,
+				time: time,
+			});
+		} else if (duration > gap) {
+			result.push({
+				freq: freq,
+				time: time,
+			});
 			result.push({
 				freq: 0,
-				time: time + duration,
+				time: time + duration - gap,
+			});
+		} else {
+			result.push({
+				freq: 0,
+				time: time,
 			});
 		}
-		time += duration + gap;
+		time += duration;
 	}
 	result.push({
 		freq: 0,
