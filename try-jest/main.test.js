4c273a56a0469b7b90ecb6415c5c7c7aa81af496
frank-deng <13382084679@163.com>
2018-10-04 21:04:47 +0800

    Updated

--- try-jest/main.test.js
+++ try-jest/main.test.js
@@ -1,15 +1,18 @@
 var tetrisAIUtil = require('./tetrisAIUtil.js');
 
+var processLine=function(line){
+	let result=[];
+	for(let n of line){
+		result.push({
+			value:n,
+		})
+	}
+	return result;
+}
 var processBoard=function(board){
 	let result=[];
-	for(let rowSrc of board){
-		let row=[];
-		for(let n of rowSrc){
-			row.push({
-				value:n,
-			})
-		}
-		result.push(row);
+	for(let row of board){
+		result.push(processLine(row));
 	}
 	return result;
 };
@@ -39,3 +42,16 @@ test('getElimateLines',()=>{
 	]);
 	expect(tetrisAIUtil.getElimateLines(board)).toBe(4);
 });
+test('_getTransition',()=>{
+	let line = processLine([0,0,0,0,0,0,0,0,0,0]);
+	expect(tetrisAIUtil._getTransition(line)).toBe(2);
+	line = processLine([0,0,0,1,0,0,0,0,0,0]);
+	expect(tetrisAIUtil._getTransition(line)).toBe(4);
+	line = processLine([1,1,0,0,1,1,0,0,1,1,0,0]);
+	expect(tetrisAIUtil._getTransition(line)).toBe(6);
+	line = processLine([0,0,0,1,0,1,0,1,0,1]);
+	expect(tetrisAIUtil._getTransition(line)).toBe(8);
+	line = processLine([1,1,1,1,1,1,1,1,1,1]);
+	expect(tetrisAIUtil._getTransition(line)).toBe(0);
+});
+
