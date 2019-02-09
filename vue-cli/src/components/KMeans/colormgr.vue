c770c6f9e0eab5c77564286bf16ae9e1d5ee725b
frank-deng <13382084679@163.com>
2019-02-09 14:20:00 +0000

    Updated style of KMeans

--- vue-cli/src/components/KMeans/colormgr.vue
+++ vue-cli/src/components/KMeans/colormgr.vue
@@ -1,44 +1,46 @@
 <template>
   <div>
     <el-button v-if='!editing' @click='startEditing'>编辑初始颜色</el-button>
-    <div v-if='editing'>
-      <ul>
-        <li class='colorItem' v-for='(color,idx) of colors' :key='idx'>
-          <el-color-picker v-model='color.value' @change='checkColor(idx, $event)'></el-color-picker>
-        </li>
-      </ul>
-      <el-button-group><!--
-        --><el-button class='textButton' type='text' icon='el-icon-plus' @click='addColor'></el-button><!--
-        --><el-button class='textButton' type='text' icon='el-icon-refresh' @click='resetColors'></el-button><!--
-        --><el-button class='textButton' type='text' icon='el-icon-download' @click='downloadPalette'></el-button><!--
-      --></el-button-group><!--
-      --><el-upload action='' class='paletteUpload'
-        :multiple='false'
-        :limit='1'
-        :show-file-list='false'
-        :auto-upload='true'
-        :before-upload='handlePaletteUpload' >
-        <el-button class='textButton' type='text' icon='el-icon-upload2' @click='resetColors'></el-button>
-      </el-upload>
+    <div v-if='editing' class='editorContainer'>
+      <el-color-picker
+        class='colorItem'
+        v-for='(color,idx) of colors'
+        :key='idx'
+        v-model='color.value'
+        @change='checkColor(idx, $event)'></el-color-picker>
+      <div class='toolBox'>
+        <el-button-group><!--
+          --><el-button class='textButton' type='text' icon='el-icon-plus' @click='addColor'></el-button><!--
+          --><el-button class='textButton' type='text' icon='el-icon-refresh' @click='resetColors'></el-button><!--
+          --><el-button class='textButton' type='text' icon='el-icon-download' @click='downloadPalette'></el-button><!--
+        --></el-button-group><!--
+        --><el-upload action='' class='paletteUpload'
+          :multiple='false'
+          :limit='1'
+          :show-file-list='false'
+          :auto-upload='true'
+          :before-upload='handlePaletteUpload' >
+          <el-button class='textButton' type='text' icon='el-icon-upload2' @click='resetColors'></el-button>
+        </el-upload>
+      </div>
     </div>
   </div>
 </template>
 <style scoped>
-ul{
-  overflow:auto;
-}
-li{
-  list-style:none;
-}
-.el-color-picker{
-  vertical-align:top;
+.editorContainer{
+  overflow:hidden;
 }
 .colorItem{
+  display:block;
   float:left;
   margin:0 4px 4px 0;
 }
 .paletteUpload{
   display:inline-block;
+  vertical-align:middle;
+}
+.toolBox{
+  clear:both;
 }
 .textButton{
   width:40px;
