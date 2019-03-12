d584de509ebb65463132e78733bea17deaf4b2d9
frank-deng <13382084679@163.com>
2019-03-12 10:26:08 +0800

    Updated

--- vue-cli/src/components/retro-ppt/retro-ppt.vue
+++ vue-cli/src/components/retro-ppt/retro-ppt.vue
@@ -2,7 +2,7 @@
   <div class='retro-ppt'>
     <addImage @upload='newImage'></addImage>
     <el-table :data='imageList'>
-      <el-table-column prop='fileName' label='文件名'></el-table-column>
+      <el-table-column prop='fileName' label='文件名' fixed='left'></el-table-column>
       <el-table-column prop='layout' label='布局方式' :width='160'>
         <template slot-scope='scope'>
           <el-select v-model='scope.row.layout'>
@@ -10,19 +10,19 @@
           </el-select>
         </template>
       </el-table-column>
-      <el-table-column prop='dither' label='抖动方式' :width='200'>
+      <el-table-column prop='dither' label='抖动方式' width='200'>
         <template slot-scope='scope'>
           <el-select v-model='scope.row.dither'>
             <el-option v-for='item of ditherSelection' :key='item.value' :value='item.value' :label='item.label'></el-option>
           </el-select>
         </template>
       </el-table-column>
-      <el-table-column label='预览' :width='200'>
+      <el-table-column label='预览' width='200'>
         <template slot-scope='scope'>
           <processImage :image='scope.row.file' :layout='scope.row.layout' :dither='scope.row.dither'></processImage>
         </template>
       </el-table-column>
-      <el-table-column fixed='right' width='160' align='right'>
+      <el-table-column width='160' align='right'>
         <template slot-scope='scope'>
           <el-button-group>
             <el-button size='small' icon='el-icon-arrow-up' @click='moveUpImage(scope.$index)'
