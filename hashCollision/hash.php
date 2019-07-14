d3f47033f137826d6e0cec633285185f117639b1
frank-deng <13382084679@163.com>
2019-07-14 15:12:25 +0800

    Delete some files

--- hashCollision/hash.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ms = microtime(true);
-$json = json_decode(file_get_contents('hashNormal.json'), true);
-echo sprintf("Parse hashNormal.json: %.3fs\n", (microtime(true) - $ms));
-
-$ms = microtime(true);
-$json = json_decode(file_get_contents('hash.json'), true); //PHP Hash Collision 烫烫烫烫烫烫烫烫
-echo sprintf("Parse hash.json: %.3fs\n", (microtime(true) - $ms));
-
--- tryasync.js
+++
@@ -1,9 +0,0 @@
-async function testSync(){
-	const response = await new Promise((resolve) =>{
-		setTimeout(()=>{
-			resolve('Miao~~~');
-		}, 1000);
-	});
-	console.log(response);
-}
-testSync();
