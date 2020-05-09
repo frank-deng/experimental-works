1ffe7e56c4ce3878c2b6d7cbdca144c022482408
frank-deng <13382084679@163.com>
2020-05-09 23:23:34 +0800

    Zone plate updated

--- vue-cli/src/views/zonePlate/zonePlate.js
+++ vue-cli/src/views/zonePlate/zonePlate.js
@@ -1,25 +1,47 @@
 export default{
   data(){
     return{
+      focus:200,
+      waveLength:1
     };
   },
   mounted(){
-    this.drawZonePlate();
+    let canvas=this.$refs.zonePlateCanvas, container=this.$refs.zonePlateContainer;
+    let resizeHandler=()=>{
+      canvas.width=container.offsetWidth;
+      canvas.height=container.offsetHeight;
+    }
+    resizeHandler();
+    window.addEventListener('resize',resizeHandler);
+
+    let running=true, offset=0;
+    let frameHandler=()=>{
+      this.drawZonePlate(offset);
+      offset-=Math.PI/10;
+      if(running){
+        requestAnimationFrame(frameHandler);
+      }
+    }
+    this.$once('hook:beforeDestroy',()=>{
+      window.removeEventListener('resize',resizeHandler);
+      running=false;
+    });
+    frameHandler();
   },
   methods:{
-    drawZonePlate(){
+    drawZonePlate(offsetCos){
       let canvas=this.$refs.zonePlateCanvas;
       let ctx=canvas.getContext('2d');
       let image=ctx.getImageData(0,0,canvas.width,canvas.height);
 
-      let f=10, cx=Math.round(image.width/2), cy=Math.round(image.height/2);
+      let f=this.focus, waveLength=this.waveLength, cx=Math.round(image.width/2), cy=Math.round(image.height/2);
       for(let y=0;y<image.height;y++){
         for(let x=0;x<image.width;x++){
           let offset=(y*canvas.width+x)*4;
 
           let r2=(x-cx)*(x-cx)+(y-cy)*(y-cy);
-          let l=Math.sqrt(f*f+r2);
-          let value=Math.round(Math.cos(l)*255);
+          let n=r2/f/waveLength;
+          let value=Math.round((Math.cos(n+offsetCos)+1)/2.0*255);
           image.data[offset]=image.data[offset+1]=image.data[offset+2]=value;
           image.data[offset+3]=255;
         }
--- vue-cli/src/views/zonePlate/zonePlate.vue
+++ vue-cli/src/views/zonePlate/zonePlate.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class='zonePlate'>
-    <canvas class='zonePlateCanvas' ref='zonePlateCanvas' width='640' height='480'></canvas>
+  <div class='zonePlate' ref='zonePlateContainer'>
+    <canvas class='zonePlateCanvas' ref='zonePlateCanvas' width='320' height='200'></canvas>
   </div>
 </template>
 <style scoped lang='less'>
+.zonePlate{
+  width:100%;
+  height:100%;
+}
 .zonePlateCanvas{
   display:block;
-  margin:0 auto;
-  border:1px solid #000000;
+  position:absolute;
+  left:0;
+  right:0;
+  top:0;
+  bottom:0;
+  width:100%;
+  height:100%;
 }
 </style>
 <script src='./zonePlate.js'></script>
