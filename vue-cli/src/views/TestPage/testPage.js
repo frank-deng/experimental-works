709495ddcd485bafb77b7f1b3c79904f46a48062
frank-deng <13382084679@163.com>
2021-04-18 19:11:53 +0800

    Cleared TestPage/testPage.js

--- vue-cli/src/views/TestPage/testPage.js
+++ vue-cli/src/views/TestPage/testPage.js
@@ -1,20 +1,9 @@
-/* global __DEV__ */
-import config from '@/config.js';
-import axios from 'axios';
-import echarts from 'echarts';
 export default{
   data(){
     return{
     };
   },
   mounted(){
-    axios.get('https://api.tianapi.com/txapi/ncovcity/index',{
-      params:{
-        key:config.key
-      }
-    }).then((resp)=>{
-      console.log(resp);
-    });
   },
   methods:{
   }
