6e0373844139341d107c7f9615a78ae294e34232
frank-deng <13382084679@163.com>
2021-07-27 21:36:35 +0800

    Lego updated

--- lego/index.js
+++ lego/index.js
@@ -4,10 +4,12 @@ const EvdevReader=require('evdev');
 const poweredUp=new PoweredUP.PoweredUP();
 const reader=new EvdevReader();
 
-let masterHub=null;
-poweredUp.on('discover',async function(hub){
+let running=true;
+async function onDiscover(hub){
+  poweredUp.stop();
+  poweredUp.off('discover',onDiscover);
+
   console.log('Discovered: '+hub.name);
-  masterHub=hub;
   await hub.connect();
   console.log('connected',hub.uuid);
   const [motorA,motorB,motorC] = await Promise.all([
@@ -17,6 +19,7 @@ poweredUp.on('discover',async function(hub){
   ]);
 
   //Calibrate
+  /*
   let minAngle=maxAngle=null;
   let calibrateFunc=(e)=>{
     let angle=Number(e.degrees);
@@ -38,50 +41,68 @@ poweredUp.on('discover',async function(hub){
   motorC.off('rotate',calibrateFunc);
 
   let mediumAngle=Math.round((maxAngle+minAngle)/2);
-  let range=(maxAngle-minAngle)/2;
-  console.log(minAngle, maxAngle, mediumAngle, range);
   motorC.gotoAngle(mediumAngle,30);
   await hub.sleep(2000);
-  motorC.resetZero();
-  motorC.gotoAngle(0,100);
+  await motorC.resetZero();
+  await motorC.gotoAngle(0,100);
+  */
 
-  motorA.setAccelerationTime(1);
-  motorA.setDecelerationTime(1);
-  motorB.setAccelerationTime(1);
-  motorB.setDecelerationTime(1);
-  motorC.setAccelerationTime(1);
-  motorC.setDecelerationTime(1);
+  await Promise.all([
+    motorA.setAccelerationTime(1),
+    motorA.setDecelerationTime(1),
+    motorB.setAccelerationTime(1),
+    motorB.setDecelerationTime(1),
+    motorC.setAccelerationTime(1),
+    motorC.setDecelerationTime(1)
+  ]);
 
-  reader.on("EV_KEY",function(data){
+  await motorC.gotoAngle(0,100);
+
+  let currentAngle=0;
+  motorC.on('rotate',(e)=>{
+    currentAngle=Number(e.degrees);
+  });
+  reader.on("EV_ABS",(data)=>{
+    if('ABS_X'==data.code){
+      let requestAngle=Math.round(90*(data.value-128)/128);
+      speed=Math.abs(currentAngle-requestAngle)/180*100;
+      speed=Math.min(Math.max(speed,1),100);
+      motorC.gotoAngle(requestAngle,speed);
+    }else if('ABS_RZ'==data.code){
+      let value=Math.round((data.value-128)*100/128);
+      motorA.setPower(value);
+      motorB.setPower(value);
+    }
+  });
+
+  reader.on("EV_KEY",(data)=>{
     if('BTN_A'==data.code && data.value){
       motorA.brake();
       motorB.brake();
       motorC.brake();
     }
-  }).on("EV_ABS",function(data){
-    if('ABS_X'==data.code){
-      let angle=Math.round(range*(data.value-128)/128);
-      motorC.gotoAngle(angle,50);
-    }
-    if('ABS_RZ'==data.code){
-      let value=data.value-128;
-      if(Math.abs(value)<1){
-        motorA.brake();
-        motorB.brake();
-      }else{
-        motorA.setPower(Math.round(100*value/128));
-        motorB.setPower(Math.round(100*value/128));
-      }
-    }
   });
-});
 
-function exitFunction(){
-  console.log('Exit.');
-  reader.close();
-  //process.exit();
-  process.kill(process.pid,'SIGKILL');
+  while(running){
+    await hub.sleep(10);
+  }
 }
+
+poweredUp.on('discover',onDiscover);
+
+process.on('SIGINT',function exitFunction(){
+  try{
+    running=false;
+    poweredUp.stop();
+    reader.close();
+    console.log('Exit.');
+  }catch(e){
+    console.error(e);
+  }finally{
+    //process.exit();
+    process.kill(process.pid,'SIGKILL');
+  }
+});
 
 //Load joystick
 console.log('Loading Joystick');
@@ -97,12 +118,9 @@ reader.search('/dev/input/by-id','event-joystick',(err,files)=>{
 
   let device=reader.open(files[0]);
   device.on('open',()=>{
-    console.log(device);
+    console.log(device.id);
     console.log('Start discovering');
     poweredUp.scan();
-    console.log('Start discovering');
-    process.on('SIGINT',exitFunction);
-    process.on('exit',exitFunction);
   });
-})
+});
 
