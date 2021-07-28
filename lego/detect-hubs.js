b7a6d8e913556735dcfcb5f944b26b59eca76c5a
frank-deng <13382084679@163.com>
2021-07-28 16:13:16 +0800

    Removed lego related code since lego related projects has its own repo.

--- lego/detect-hubs.js
+++
@@ -1,9 +0,0 @@
-const PoweredUP=require('node-poweredup');
-const poweredUp=new PoweredUP.PoweredUP();
-
-poweredUp.on('discover',(hub)=>{
-  console.log(`Hub Discovered, ID: ${hub.uuid}    Name: ${hub.name}`);
-});
-console.log('Start scanning...');
-poweredUp.scan();
-
