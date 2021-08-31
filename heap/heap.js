ad683e473809dc58dd4e21077bfacb1247ae7103
frank-deng <13382084679@163.com>
2021-08-31 19:07:19 +0800

    Heap added

--- heap/heap.js
+++ heap/heap.js
@@ -1,40 +1,99 @@
 class Heap{
-    type=0; //0=最小堆，1=最大堆
-    data=[];
-    constructor(type){
-        this.type=type ? 1 : 0;
+  static MIN_HEAP=Symbol();
+  static MAX_HEAP=Symbol();
+  data=[];
+  constructor(type=undefined){
+    if(undefined===type){
+      throw new Error('Heap type not specified.');
     }
-    __parentAddr(idx){
-        if(idx<=0){
-            return undefined;
-        }
-        return (idx-1)>>1;
+    if(Heap.MIN_HEAP!==type && Heap.MAX_HEAP!==type){
+      throw new TypeError('Invalid heap type.');
     }
-    push(value){
-        let addr=this.data.length;
-        this.data.push(value);
-        while(addr){
-            let parentAddr=this.__parentAddr(addr);
-            let parentValue=this.data[parentAddr];
-            if((!this.type && parentValue>value) || (this.type && parentValue<value)){
-                break;
-            }
-        }
-        //链式操作用
-        return this;
-    }
-    pop(){
-        if(!this.data.length){
-            return undefined;
-        }
-        let result=this.data[0];
-    }
-}
-Object.defineProperty(Heap,'length',{
-    configurable:false,
-    enumerable:false,
-    get(){
+    //Works similar to string.length or array.length
+    Object.defineProperty(this,'length',{
+      configurable:false,
+      enumerable:false,
+      get(){
         return this.data.length;
+      }
+    });
+    this.type=type;
+  }
+  __parentAddr(idx){
+    return idx<=0 ? undefined : (idx-1)>>1;
+  }
+  __leftChildAddr(idx){
+    return (idx<<1)+1;
+  }
+  __rightChildAddr(idx){
+    return (idx<<1)+2;
+  }
+  peek(){
+    return this.data[0];
+  }
+  push(value){
+    let addr=this.data.length;
+    this.data.push(value);
+    while(addr){
+      let parentAddr=this.__parentAddr(addr);
+      let parentValue=this.data[parentAddr];
+      if((Heap.MIN_HEAP===this.type && parentValue<=value)
+        || (Heap.MAX_HEAP===this.type && parentValue>=value)){
+        break;
+      }
+      let temp=this.data[addr];
+      this.data[addr]=this.data[parentAddr];
+      this.data[parentAddr]=temp;
+      addr=parentAddr;
     }
-});
+    //Chaining use
+    return this;
+  }
+  pop(){
+    let arr=this.data;
+    //Heap is empty now
+    if(!arr.length){
+      return undefined;
+    }
+
+    //Get the root item and its value
+    let result=arr[0];
+    let tailItem=arr.pop();
+
+    //Heap is empty now
+    if(!arr.length){
+      return result;
+    }
+
+    //Move the last item of the heap to top
+    arr[0]=tailItem;
+    
+    //Move downwards if needed
+    let addr=0,len=arr.length;
+    while(addr<len){
+      let leftChildAddr=this.__leftChildAddr(addr),
+        rightChildAddr=this.__rightChildAddr(addr),
+        currValue=arr[addr],
+        leftValue=arr[leftChildAddr],
+        rightValue=arr[rightChildAddr],
+        target=null;
+      if(Heap.MIN_HEAP===this.type
+        && (currValue>leftValue || currValue>rightValue)){
+        target=leftValue<rightValue ? leftChildAddr : rightChildAddr;
+      }else if(Heap.MAX_HEAP===this.type
+        && (currValue<leftValue || currValue<rightValue)){
+        target=leftValue>rightValue ? leftChildAddr : rightChildAddr;
+      }
+      if(null===target){
+        break;
+      }
+      let temp=arr[addr];
+      arr[addr]=arr[target];
+      arr[target]=temp;
+      addr=target;
+    }
+    return result;
+  }
+}
 module.exports=Heap;
+
--- heap/heap.test.js
+++ heap/heap.test.js
@@ -1,2 +1,73 @@
-const Heap=require('heap');
 const assert=require('assert');
+const Heap=require('./heap');
+
+describe('堆数据结构',function(){
+  it('基本功能测试',function(){
+    let heap=new Heap(Heap.MIN_HEAP);
+    heap.push(3);
+    heap.push(4);
+    heap.push(1);
+    assert.deepStrictEqual(heap.data,[1,4,3]);
+    assert.strictEqual(heap.pop(),1);
+  });
+  it('各种异常操作',function(){
+    assert.throws(()=>new Heap(),Error,'Heap type not specified.');
+    assert.throws(()=>new Heap(666),TypeError,'Invalid heap type.');
+    let heap=new Heap(Heap.MIN_HEAP);
+    assert.strictEqual(heap.pop(),undefined);
+    heap.push(3);
+    heap.push(4);
+    heap.push(1);
+    heap.length=666;
+    assert.strictEqual(heap.length,3);
+  });
+  it('进堆',function(){
+    let heap=new Heap(Heap.MIN_HEAP);
+    heap.push(3);
+    heap.push(4);
+    heap.push(2);
+    heap.push(6);
+    heap.push(5);
+    assert.deepStrictEqual(heap.data,[2,4,3,6,5]);
+    heap.push(1);
+    assert.deepStrictEqual(heap.data,[1,4,2,6,5,3]);
+    let heap2=new Heap(Heap.MAX_HEAP);
+    heap2.push(3);
+    heap2.push(4);
+    heap2.push(2);
+    heap2.push(6);
+    heap2.push(5);
+    heap2.push(1);
+    assert.deepStrictEqual(heap2.data,[6,5,2,3,4,1]);
+  });
+  it('出堆',function(){
+    let heap=new Heap(Heap.MIN_HEAP);
+    heap.push(3);
+    heap.push(4);
+    heap.push(2);
+    heap.push(6);
+    heap.push(5);
+    heap.push(1);
+    assert.strictEqual(heap.pop(),1);
+    assert.deepStrictEqual(heap.data,[2,4,3,6,5]);
+    heap=new Heap(Heap.MIN_HEAP);
+    heap.push(1);
+    heap.push(2);
+    heap.push(3);
+    heap.push(4);
+    heap.push(5);
+    heap.push(6);
+    heap.push(70);
+    assert.strictEqual(heap.pop(),1);
+    assert.deepStrictEqual(heap.data,[2,4,3,70,5,6]);
+    let heap2=new Heap(Heap.MAX_HEAP);
+    heap2.push(3);
+    heap2.push(4);
+    heap2.push(2);
+    heap2.push(6);
+    heap2.push(5);
+    heap2.push(1);
+    assert.strictEqual(heap2.pop(),6);
+    assert.deepStrictEqual(heap2.data,[5,4,2,3,1]);
+  });
+}); 
