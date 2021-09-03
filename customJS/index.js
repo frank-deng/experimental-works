c5c40fbf559bbe5a92fa0509337bc713e580970b
frank-deng <13382084679@163.com>
2021-09-03 18:46:41 +0800

    手写JS基本功能

---
+++ customJS/index.js
@@ -0,0 +1,102 @@
+function createNew(){
+  let constructor=arguments[0];
+  if('function'!==typeof(constructor)){
+    throw TypeError('Function is required');
+  }
+  let args=[];
+  for(let i=1; i<arguments.length; i++){
+    args.push(arguments[i]);
+  }
+
+  let targetObj={};
+  targetObj.__proto__=constructor.prototype;
+  let result=apply(constructor,targetObj,args);
+  if('object'===typeof(result)
+    || 'function'===typeof(result)){
+    return result;
+  }
+  return targetObj;
+}
+function apply(func,target,args){
+  if('function'!==typeof(func)){
+    throw TypeError('Function is required');
+  }
+  let ctx={};
+  try{
+    ctx=target||global;
+  }catch(e){}
+  if(undefined===args){
+    args=[];
+  }else if(!Array.isArray(args)){
+    throw TypeError('array required as parameters');
+  }
+  //要给目标对象加上个key，key为随机值，值为目标函数
+  let key=String(Math.random());
+  ctx[key]=func;
+  let result=ctx[key](...args);
+  delete ctx[key];
+  return result;
+}
+function call(){
+  let func=arguments[0], target=arguments[1];
+  if('function'!==typeof(constructor)){
+    throw TypeError('Function is required');
+  }
+  let args=[];
+  for(let i=2; i<arguments.length; i++){
+    args.push(arguments[i]);
+  }
+  if('function'!==typeof(func)){
+    throw TypeError('Function is required');
+  }
+  let ctx={};
+  try{
+    ctx=target||global;
+  }catch(e){}
+  //要给目标对象加上个key，key为随机值，值为目标函数
+  let key=String(Math.random());
+  ctx[key]=func;
+  let result=ctx[key](...args);
+  delete ctx[key];
+  return result;
+}
+function bind(){
+  let func=arguments[0], target=arguments[1];
+  if('function'!==typeof(func)){
+    throw TypeError('Function is required');
+  }
+  try{
+    target=target||global;
+  }catch(e){
+    target={};
+  }
+  let args=[];
+  for(let i=2; i<arguments.length; i++){
+    args.push(arguments[i]);
+  }
+  return function F(){
+    if(!instanceOf(this,F)){
+      return apply(func,target,args);
+    }
+    return createNew(func,...args,...arguments);
+  }
+}
+function instanceOf(left,right){
+  let target=right.prototype;
+  left=left.__proto__;
+  while(left){
+    if(left===target){
+      return true;
+    }
+    left=left.__proto__;
+  }
+  return false;
+}
+module.exports={
+  createNew,
+  apply,
+  call,
+  bind,
+  instanceOf
+};
+
---
+++ customJS/test.js
@@ -0,0 +1,133 @@
+const assert=require('assert');
+const {
+  createNew,
+  apply,
+  call,
+  bind,
+  instanceOf
+}=require('./index');
+
+describe('createNew()',function(){
+  it('Functionality test',function(){
+    function TestClass(a,b){
+      this.a=a;
+      this.b=b;
+    }
+    TestClass.prototype={
+      constructor:TestClass,
+      calc(){
+        return this.a+this.b;
+      }
+    }
+    let testInstance=createNew(TestClass,1,2);
+    let testInstance2=new TestClass(1,2);
+    assert.strictEqual(testInstance.calc(),3);
+    assert.strictEqual(testInstance2.calc(),3);
+    assert.strictEqual(testInstance.calc,testInstance2.calc);
+  });
+  it('Constructor return non-object value',function(){
+    function TestClass(value){
+      return value;
+    }
+    let testInstance=createNew(TestClass,'666');
+    let testInstance2=new TestClass('666');
+    assert.strictEqual(testInstance instanceof TestClass,true);
+    assert.strictEqual(testInstance2 instanceof TestClass,true);
+  });
+  it('Constructor return object value',function(){
+    function TestClass(value){
+      return {
+        value
+      };
+    }
+    let testInstance=createNew(TestClass,'666');
+    let testInstance2=new TestClass('666');
+    assert.deepStrictEqual(testInstance,{value:'666'});
+    assert.deepStrictEqual(testInstance2,{value:'666'});
+  });
+});
+describe('call()和apply()',function(){
+  it('Basic Test',function(){
+    function Sum(initVal){
+      this.value=initVal;
+    }
+    Sum.prototype={
+      constructor:Sum,
+      add(value){
+        this.value+=value;
+        return this;
+      },
+      getValue(){
+        return this.value;
+      }
+    }
+    let sum0=createNew(Sum,1);
+    let sum1=new Sum(2);
+    assert.strictEqual(apply(Sum.prototype.getValue,sum0),1);
+    assert.strictEqual(apply(Sum.prototype.getValue,sum1),2);
+    apply(Sum.prototype.add,sum0,[2]);
+    apply(Sum.prototype.add,sum1,[3]);
+    assert.strictEqual(apply(Sum.prototype.getValue,sum0),3);
+    assert.strictEqual(apply(Sum.prototype.getValue,sum1),5);
+    call(Sum.prototype.add,sum0,10);
+    call(Sum.prototype.add,sum1,20);
+    assert.strictEqual(call(Sum.prototype.getValue,sum0),13);
+    assert.strictEqual(call(Sum.prototype.getValue,sum1),25);
+  });
+});
+describe('bind()',function(){
+  it('Basic test',function(){
+    function Sum(initVal){
+      this.value=initVal;
+    }
+    Sum.prototype={
+      ...Sum.prototype,
+      add(value){
+        this.value+=value;
+        return this;
+      },
+      getValue(){
+        return this.value;
+      }
+    }
+    let sum0=createNew(Sum,1);
+    let sum1=createNew(Sum,2);
+    func0=sum0.getValue.bind(sum0);
+    func1=bind(sum1.getValue,sum1);
+    assert.strictEqual(func0(),1);
+    assert.strictEqual(func1(),2);
+    assert.strictEqual(Sum.prototype.getValue(),undefined);
+  });
+  it('Create object with bind()',function(){
+    function Sum(initVal){
+      this.value=initVal;
+    }
+    Object.assign(Sum.prototype,{
+      add(value){
+        this.value+=value;
+        return this;
+      },
+      getValue(){
+        return this.value;
+      }
+    });
+    let sum0=createNew(bind(Sum,{a:2,value:666},1));
+    let sum1=createNew(bind(Sum,{a:2,value:666}),1);
+    let sum2=new (Sum.bind({a:2,value:666},1));
+    let sum3=new (Sum.bind({a:2,value:666}))(1);
+    assert.strictEqual(sum0.a,undefined);
+    assert.strictEqual(sum1.a,undefined);
+    assert.strictEqual(sum2.a,undefined);
+    assert.strictEqual(sum3.a,undefined);
+    assert.strictEqual(sum0.getValue(),1);
+    assert.strictEqual(sum1.getValue(),1);
+    assert.strictEqual(sum2.getValue(),1);
+    assert.strictEqual(sum3.getValue(),1);
+  });
+});
+describe('instanceOf()',function(){
+  it('Basic test',function(){
+    assert.strictEqual(instanceOf([],Array), [] instanceof Array);
+  });
+});
+
