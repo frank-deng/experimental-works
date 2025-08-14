2ee1532c364c8598c3c00507629bcad8926bb7a8
frank-deng <13382084679@163.com>
2025-08-14 14:50:55 +0800

    newslist added

---
+++ servers/web/news.py
@@ -0,0 +1,57 @@
+import asyncio
+import aiohttp
+import logging
+import hashlib
+import base64
+from aiohttp.web import Request
+from aiohttp.web import Response
+from aiohttp_jinja2 import render_string
+from util import Logger
+from pprint import pformat
+
+class NewsManager(Logger):
+    __host='https://apis.tianapi.com'
+    def __init__(self,key):
+        self.__key=key
+        self.__newsLinks={}
+        self.__newsLinksLock=asyncio.Lock()
+
+    async def __fetch(self,path,params_in=None):
+        params={'key':self.__key}
+        if params_in is not None:
+            params.update(params_in)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f'{self.__host}/{path}',data=params) as response:
+                return await response.json()
+
+    async def newsList(self):
+        res=await self.__fetch('guonei/index',{'num':50})
+        res=res['result']['newslist']
+        async with self.__newsLinksLock:
+            for item in res:
+                item['id']=item['id'][:12]
+                self.__newsLinks[item['id']]=item['url']
+        return res
+
+    async def newsDetail(self,newsid):
+        newsLink=None
+        async with self.__newsLinksLock:
+            newsLink=self.__newsLinks.get(newsid,None)
+        if newsLink is None:
+            return None
+
+async def news_handler(req:Request):
+    config=req.app['config']
+    context={
+        'header':'今日热点',
+        'title':'今日热点',
+        'news':await req.app['newsManager'].newsList()
+    }
+    encoding=config['web']['encoding']
+    return Response(
+        body=render_string("newslist.html",req,context).encode(encoding,errors='replace'),
+        headers={
+            'content-type':f"text/html; charset={encoding}"
+        }
+    )
+
