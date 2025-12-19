5aa15fe6e031e0532f4579b38218a757fba94c82
frank-deng <13382084679@163.com>
2025-12-19 21:38:35 +0800

    Update blog-maker2

--- blog-maker2/blog.py
+++ blog-maker2/blog.py
@@ -2,14 +2,20 @@
 
 import sys
 import os
-import click
+import re
+import glob
 import tomllib
-from util import Logger
+import click
+import dateutil
+from datetime import datetime
 
-class BlogMaker(Logger):
+class BlogMaker:
+    PARSE_META=0
+    PARSE_CONTENT=1
     def __init__(self,config_file='blog.toml',output=None):
         self.__config={
             'encoding':"GBK",
+            'markdownPath':'posts',
             'defaultLayout':"post",
             'outputPath':"dist",
             'font':{
@@ -27,8 +33,42 @@ class BlogMaker(Logger):
         if output is not None:
             self.__config['outputPath']=output
 
+    def process_markdown(self,fname,fp):
+        step=BlogMaker.PARSE_META
+        fnparsed=re.findall(r'^(19\d\d|20\d\d)(0\d|1[0-2])([0-2]\d|3[01])_([0-1]\d|2[0-3])([0-5]\d)(.*)\.md$',fname)
+        if fnparsed is None:
+            click.echo(click.style(f'Misformatted filename {fname}',fg='yellow'))
+            return None,None
+        meta={
+            'Title':'',
+            'Tags':[],
+            'Date':datetime(*[int(n) for n in fnparsed[0][:5]])
+        }
+        content=''
+        for line in fp:
+            if step!=BlogMaker.PARSE_META:
+                content+=line
+                continue
+            line=line.strip()
+            if line=='---':
+                step=BlogMaker.PARSE_CONTENT
+                continue
+            key,val=line.split(':')
+            key=key.strip()
+            val=val.strip()
+            if key=='Tags':
+                meta[key]=[v.strip() for v in val.split(',')]
+            if key=='Date':
+                meta[key]=dateutil.parse(val)
+            else:
+                meta[key]=val
+
     def parse(self):
-        click.echo(self.__config)
+        files=[f for f in glob.glob(os.path.join(self.__config['markdownPath'],'*.md')) if os.path.isfile(f)]
+        files.reverse()
+        for file in files:
+            with open(file) as fp:
+                self.process_markdown(file,fp)
 
 
 @click.group(invoke_without_command=True)
