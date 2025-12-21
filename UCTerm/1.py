71e6e8b94f242d894b3b5e91b775e9c67c6ae085
frank-deng <13382084679@163.com>
2025-12-21 23:58:43 +0800

    Updated UCTerm

--- UCTerm/1.py
+++ UCTerm/1.py
@@ -1,13 +1,24 @@
 import pygame
+import pygame.freetype
 import sys
+import time
 
 class Main:
     __running=True
     def __init__(self):
         pygame.init()
         self.__scr = pygame.display.set_mode((640, 400))
+        self.__tl=pygame.Surface((640,400),pygame.SRCALPHA)
+        self.__gl=pygame.Surface((640,400))
+        self.__font=pygame.freetype.Font('unifont-17.0.03.pcf',16)
 
     def run(self):
+        self.__tl.fill((0,0,0,0))
+        self.__gl.fill((0x70,0,0))
+        self.__font.render_to(self.__tl,(0,0),'中西文超级终端',(255,255,255,255))
+        self.__scr.blit(self.__gl,(0,0))
+        self.__scr.blit(self.__tl,(0,0))
+        pygame.display.flip()
         while self.__running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -15,6 +26,7 @@ class Main:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.__running = False
+            time.sleep(0)
 
 
 if __name__=='__main__':
