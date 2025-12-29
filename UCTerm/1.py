c790a8b8dba6b1ffd4956c0e9b018c1730c6ea75
frank-deng <13382084679@163.com>
2025-12-29 18:18:39 +0800

    Print text with attr and color

--- UCTerm/1.py
+++ UCTerm/1.py
@@ -74,13 +74,104 @@ class TextLayer(FontManager):
     ATTR_BLINK=4
     ATTR_UNDERLINE=8
     ATTR_STRIKE=16
+    def __write_ram(self,char,part=0):
+        offset=self.__row*self.__cols+self.__col
+        self._textRam[offset]=char
+        self._colorRam[offset]=(self.__bg<<8)|self.__fg
+        attr=0
+        if part==1:
+            attr|=self.ATTR_RIGHT_PART
+        if self.__inverse:
+            attr|=self.ATTR_INVERSE
+        if self.__blink:
+            attr|=self.ATTR_BLINK
+        if self.__underline:
+            attr|=self.ATTR_UNDERLINE
+        if self.__strike:
+            attr|=self.ATTR_STRIKE
+        self._attrRam[offset]=attr
+
+        self.__col+=1
+        if self.__col>=self.__cols:
+            self.__col=0
+            self.__row=(self.__row+1)%self.__rows
+
+    def __get_bitmap_pos(self,row,col):
+        offset=row*self.__cols+col
+        attr=self._attrRam[offset]
+        if (attr & self.ATTR_BLINK) and self.__counter>30:
+            return None
+        bitmap_grp=self.get(self._textRam[offset])
+        if bitmap_grp is None:
+            return None
+        bitmap=None
+        if (attr & self.ATTR_RIGHT_PART) and len(bitmap_grp)>0:
+            bitmap=bitmap_grp[1][:]
+        else:
+            bitmap=bitmap_grp[0][:]
+        if attr & self.ATTR_INVERSE:
+            bitmap=bytes([(~v)&0xff for v in bitmap])
+        if attr & self.ATTR_UNDERLINE:
+            bitmap[15]=0xff
+        if attr & self.ATTR_STRIKE:
+            bitmap[7]=0xff
+        return bitmap
+
+    def __draw_bitmap(self,bitmap,x0,y0,fg,bg=None):
+        for y in range(16):
+            for x in range(8):
+                if (1<<(7-x)) & bitmap[y]:
+                    if fg is not None:
+                        self.__surface.set_at((x0+x,y0+y),fg)
+                else:
+                    if bg is not None:
+                        self.__surface.set_at((x0+x,y0+y),bg)
+        return True
+
+    def __get_color(self,idx):
+        if idx==self.__colorKey:
+            return None
+        try:
+            return self.__palette[idx]
+        except KeyError:
+            return None
+
     def __init__(self,surface):
         super().__init__()
         self.__surface=surface
-        self.__textRam=array.array('H',[0]*(80*30))
-        self.__colorRam=array.array('H',[0]*(80*30))
-        self.__attrRam=array.array('B',[0]*(80*30))
+        self._textRam=array.array('H',[0]*(80*30))
+        self._colorRam=array.array('H',[0]*(80*30))
+        self._attrRam=array.array('B',[0]*(80*30))
         self.mode=self.MODE_80_25
+
+    def text(self,s):
+        for ch in s:
+            ch=ord(ch)
+            bitmap=self.get(ch)
+            self.__write_ram(ch)
+            if len(bitmap)>1:
+                self.__write_ram(ch,1)
+
+    def default_attr(self):
+        self.__fg=7
+        self.__bg=0
+        self.__inverse=False
+        self.__blink=False
+        self.__underline=False
+        self.__strike=False
+
+    def render(self):
+        self.__counter=(self.__counter+1) % 60
+        for y in range(self.__rows):
+            for x in range(self.__cols):
+                bitmap=self.__get_bitmap_pos(y,x)
+                if bitmap is None:
+                    continue
+                color=self._colorRam[y*self.__cols+x]
+                bg=(color>>8) & 0xff
+                fg=color & 0xff
+                self.__draw_bitmap(bitmap,x*8,y*16,
+                    self.__get_color(fg),self.__get_color(bg))
 
     @property
     def mode(self):
@@ -99,9 +190,9 @@ class TextLayer(FontManager):
         self.__underline=False
         self.__strike=False
         for i in range(80*30):
-            self.__textRam[i]=0
-            self.__colorRam[i]=7
-            self.__attrRam[i]=0
+            self._textRam[i]=0
+            self._colorRam[i]=7
+            self._attrRam[i]=0
         if mode==self.MODE_80_25:
             self.__cols=80
             self.__rows=25
@@ -143,89 +234,6 @@ class TextLayer(FontManager):
             self.__palette.append((v,v,v,0xff))
         self.__mode=mode
 
-    def __write_ram(self,char,part=0):
-        offset=self.__row*self.__cols+self.__col
-        self.__textRam[offset]=char
-        self.__colorRam[offset]=(self.__bg<<8)|self.__fg
-        attr=0
-        if part==1:
-            attr|=self.ATTR_RIGHT_PART
-        if self.__inverse:
-            attr|=self.ATTR_INVERSE
-        if self.__blink:
-            attr|=self.ATTR_BLINK
-        if self.__underline:
-            attr|=self.ATTR_UNDERLINE
-        if self.__strike:
-            attr|=self.ATTR_STRIKE
-        self.__attrRam[offset]=attr
-
-        self.__col+=1
-        if self.__col>=self.__cols:
-            self.__col=0
-            self.__row=(self.__row+1)%self.__rows
-
-    def __get_bitmap_pos(self,row,col):
-        offset=row*self.__cols+col
-        attr=self.__attrRam[offset]
-        if (attr & self.ATTR_BLINK) and self.__counter>30:
-            return None
-        bitmap_grp=self.get(self.__textRam[offset])
-        if bitmap_grp is None:
-            return None
-        bitmap=None
-        if (attr & self.ATTR_RIGHT_PART) and len(bitmap_grp)>0:
-            bitmap=bitmap_grp[1][:]
-        else:
-            bitmap=bitmap_grp[0][:]
-        if attr & self.ATTR_INVERSE:
-            bitmap=bytes([(~v)&0xff for v in bitmap])
-        if attr & self.ATTR_UNDERLINE:
-            bitmap[15]=0xff
-        if attr & self.ATTR_STRIKE:
-            bitmap[7]=0xff
-        return bitmap
-
-    def __draw_bitmap(self,bitmap,x0,y0,fg,bg=None):
-        for y in range(16):
-            for x in range(8):
-                if (1<<(7-x)) & bitmap[y]:
-                    if fg is not None:
-                        self.__surface.set_at((x0+x,y0+y),fg)
-                else:
-                    if bg is not None:
-                        self.__surface.set_at((x0+x,y0+y),bg)
-        return True
-
-    def __get_color(self,idx):
-        if idx==self.__colorKey:
-            return None
-        try:
-            return self.__palette[idx]
-        except KeyError:
-            return None
-
-    def text(self,s):
-        for ch in s:
-            ch=ord(ch)
-            bitmap=self.get(ch)
-            self.__write_ram(ch)
-            if len(bitmap)>1:
-                self.__write_ram(ch,1)
-
-    def render(self):
-        self.__counter=(self.__counter+1) % 60
-        for y in range(self.__rows):
-            for x in range(self.__cols):
-                bitmap=self.__get_bitmap_pos(y,x)
-                if bitmap is None:
-                    continue
-                color=self.__colorRam[y*self.__cols+x]
-                bg=(color>>8) & 0xff
-                fg=color & 0xff
-                self.__draw_bitmap(bitmap,x*8,y*16,
-                    self.__get_color(fg),self.__get_color(bg))
-
     @property
     def pos(self):
         return (self.__row,self.__col)
@@ -255,7 +263,7 @@ class TextLayer(FontManager):
         return self.__fg
 
     @fg.setter
-    def row(self,v):
+    def fg(self,v):
         self.__fg=v
 
     @property
@@ -263,8 +271,16 @@ class TextLayer(FontManager):
         return self.__bg
 
     @bg.setter
-    def row(self,v):
+    def bg(self,v):
         self.__bg=v
+
+    @property
+    def colorkey(self):
+        return self.__colorKey
+
+    @colorkey.setter
+    def colorkey(self,v):
+        self.__colorKey=v
 
 
 class Main:
@@ -278,12 +294,18 @@ class Main:
         self.__tl=TextLayer(self.__scr)
 
     def run(self):
+        self.__tl.pos=(24,0)
+        self.__tl.fg=0
+        self.__tl.bg=7
+        self.__tl.text(' '*80)
+        self.__tl.fg=7
+        self.__tl.bg=0
+        self.__tl.pos=(0,0)
+        for i in range(10):
+            self.__tl.text('我的Python')
         while self.__running:
             self.__scr.fill((0x70,0,0))
             self.__scr.blit(self.__gl,(0,0))
-            for i in range(20):
-                self.__tl.text('我的Python')
-
             self.__tl.render()
             pygame.display.flip()
             for event in pygame.event.get():
