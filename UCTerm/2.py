8f847a098ee7eaf7d19f499596f3cfe868134ecd
frank-deng <13382084679@163.com>
2026-01-05 10:36:26 +0800

    Rearrange opengl code

--- UCTerm/2.py
+++ UCTerm/2.py
@@ -85,9 +85,9 @@ class FontLoader:
         return self.__texture_id
 
 
-class Screen:
+class GLUtil:
     @staticmethod
-    def __ortho(left, right, bottom, top, near, far):
+    def ortho(left=-1, right=1, bottom=-1, top=1, near=-1, far=1):
         return np.array([
             [2.0/(right-left), 0.0, 0.0, -(right+left)/(right-left)],
             [0.0, 2.0/(top-bottom), 0.0, -(top+bottom)/(top-bottom)],
@@ -95,7 +95,8 @@ class Screen:
             [0.0, 0.0, 0.0, 1.0]
         ], dtype=np.float32)
 
-    def __create_shader(self):
+    @staticmethod
+    def shader_fullscr_texture():
         vertex_shader="""
         #version 330 core
         layout(location = 0) in vec2 position;
@@ -123,7 +124,122 @@ class Screen:
             compileShader(fragment_shader,GL_FRAGMENT_SHADER)
         )
 
-    def __create_shader_textlayer(self):
+    def create_texture(width,height):
+        texture_id=glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D,texture_id)
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,
+            GL_UNSIGNED_BYTE,None)
+        fbo = glGenFramebuffers(1)
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo)
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+            GL_TEXTURE_2D, texture_id, 0)
+        glBindTexture(GL_TEXTURE_2D, 0)
+        glBindFramebuffer(GL_FRAMEBUFFER, 0)
+        return texture_id,fbo
+
+    def vao_fullscr():
+        vertices = np.array([
+            -1,-1,0.0,0.0,
+            1,-1,1.0,0.0,
+            1,1,1.0,1.0,
+            -1,1,0.0,1.0,
+        ], dtype=np.float32)
+        indices = np.array([0, 1, 2, 0, 2, 3], dtype=np.uint32)
+
+        vao = glGenVertexArrays(1)
+        vbo = glGenBuffers(1)
+        ebo = glGenBuffers(1)
+
+        glBindVertexArray(vao)
+        glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * 4, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * 4, ctypes.c_void_p(2 * 4))
+        glEnableVertexAttribArray(1)
+
+        glBindVertexArray(0)
+        return vao
+
+
+class Layer:
+    def _create_shader(self):
+        return GLUtil.shader_fullscr_texture()
+
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+        self._shader=self._create_shader()
+        glUseProgram(self._shader)
+        glUniformMatrix4fv(
+            glGetUniformLocation(self._shader,"projection"),
+            1,GL_FALSE,GLUtil.ortho()
+        )
+        self._texture,self._fbo=GLUtil.create_texture(width,height)
+        self._vao=GLUtil.vao_fullscr()
+        error = glGetError()
+        if error != GL_NO_ERROR:
+            raise RuntimeError(f"OpenGL error during initialization: {error}")
+
+    def _render(self):
+        pass
+
+    def render(self):
+        glUseProgram(self._shader)
+        glBindFramebuffer(GL_FRAMEBUFFER, self._fbo)
+        glFramebufferTexture2D(GL_FRAMEBUFFER,GL_COLOR_ATTACHMENT0, 
+            GL_TEXTURE_2D,self._texture,0)
+        glViewport(0, 0, self.width, self.height)
+        glClearColor(0, 0, 0, 1)
+        glClear(GL_COLOR_BUFFER_BIT)
+        self._render()
+        glBindVertexArray(self._vao)
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+        glBindFramebuffer(GL_FRAMEBUFFER, 0)
+
+    @property
+    def texture(self):
+        return self._texture
+
+
+class GraphicLayer(Layer):
+    def __init_layer(self):
+        texture_data=np.zeros((400,640,4),dtype=np.uint8)
+        for y in range(400):
+            for x in range(640):
+                if x==0 or x==639 or y==0 or y==399:
+                    texture_data[y,x]=[255,0,0,255]
+                elif x==1 or x==638 or y==1 or y==398:
+                    texture_data[y,x]=[255,255,255,255]
+                elif y&1==0:
+                    texture_data[y,x] = [255,0,0,255]
+                else:
+                    texture_data[y,x] = [0,255,0,255]
+        glBindTexture(GL_TEXTURE_2D,self._texture)
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,self.width,self.height,0,
+            GL_RGBA,GL_UNSIGNED_BYTE,texture_data)
+        glBindTexture(GL_TEXTURE_2D, 0)
+            
+    def __init__(self,width,height):
+        super().__init__(width,height)
+        self.__init_layer()
+
+    def render(self):
+        pass
+
+
+class TextLayer(Layer):
+    def __init__(self,width,height):
+        super().__init__(width,height)
+
+    def _create_shader(self):
         vertex_shader="""
         #version 330 core
         layout(location = 0) in vec2 position;
@@ -159,117 +275,45 @@ class Screen:
             compileShader(fragment_shader,GL_FRAGMENT_SHADER)
         )
 
-    def __create_texture(self):
-        texture_data=np.zeros((400,640,4),dtype=np.uint8)
-        for y in range(400):
-            for x in range(640):
-                if x==0 or x==639 or y==0 or y==399:
-                    texture_data[y,x]=[255,0,0,255]
-                elif x==1 or x==638 or y==1 or y==398:
-                    texture_data[y,x]=[255,255,255,255]
-                elif y&1==0:
-                    texture_data[y,x] = [255,0,0,255]
-                else:
-                    texture_data[y,x] = [0,255,0,255]
+    def _render(self):
+        glUniform2f(glGetUniformLocation(self._shader,'resolution'),self.width,self.height)
+        glUniform2i(glGetUniformLocation(self._shader,'cell_size'),8,16)
 
-        # 生成纹理
-        texture_id=glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D,texture_id)
 
-        # 设置纹理参数
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE)
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE)
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
-
-        # 加载纹理数据
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 640, 400, 0,
-            GL_RGBA, GL_UNSIGNED_BYTE, texture_data)
-
-        # 生成mipmap（可选）
-        #glGenerateMipmap(GL_TEXTURE_2D)
-
-        glBindTexture(GL_TEXTURE_2D, 0)
-        return texture_id
-    
-    def __create_rect_vao(self):
-        vertices = np.array([
-            -1,-1,0.0,0.0,
-            1,-1,1.0,0.0,
-            1,1,1.0,1.0,
-            -1,1,0.0,1.0,
-        ], dtype=np.float32)
-        indices = np.array([0, 1, 2, 0, 2, 3], dtype=np.uint32)
-
-        vao = glGenVertexArrays(1)
-        vbo = glGenBuffers(1)
-        ebo = glGenBuffers(1)
-
-        glBindVertexArray(vao)
-        glBindBuffer(GL_ARRAY_BUFFER, vbo)
-        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
-
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * 4, ctypes.c_void_p(0))
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * 4, ctypes.c_void_p(2 * 4))
-        glEnableVertexAttribArray(1)
-
-        glBindVertexArray(0)
-        return vao
-
+class Screen:
     def __init__(self,vw=640,vh=400,width=640,height=400):
-        self.width=width
-        self.height=height
-        glViewport(0,0,vw,vh)
-        self.__shader=self.__create_shader()
-        self.__shader_text=self.__create_shader_textlayer()
-
+        self._vw,self._vh=vw,vh
+        self.__gLayer=GraphicLayer(width,height)
+        self.__tLayer=TextLayer(width,height)
+        self.__shader=GLUtil.shader_fullscr_texture()
+        self.__texture,self.__fbo=GLUtil.create_texture(width,height)
         glUseProgram(self.__shader)
-        proj_loc = glGetUniformLocation(self.__shader, "projection")
-        glUniformMatrix4fv(proj_loc, 1, GL_FALSE,
-            self.__class__.__ortho(-1,1,-1,1,-1,1))
-        self.__rect_vao=self.__create_rect_vao()
-        self.__texture_id=self.__create_texture()
-        glUseProgram(self.__shader)
-        glUniform1i(glGetUniformLocation(self.__shader, "textureSampler"), 0)
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.__shader,"projection"),
+            1,GL_FALSE,GLUtil.ortho()
+        )
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
-        glUseProgram(self.__shader_text)
-        proj_loc = glGetUniformLocation(self.__shader_text, "projection")
-        glUniformMatrix4fv(proj_loc, 1, GL_FALSE,
-            self.__class__.__ortho(-1,1,-1,1,-1,1))
-        fontLoader=FontLoader()
-        self.__font_texture_id=fontLoader.texture_id
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
-        error = glGetError()
-        if error != GL_NO_ERROR:
-            print(f"OpenGL error during initialization: {error}")
+        self.__vao=GLUtil.vao_fullscr()
 
     def update(self):
+        self.__tLayer.render()
+
+        glUseProgram(self.__shader)
+        glViewport(0,0,self._vw,self._vh)
+
         glClearColor(0, 0, 0, 1)
         glClear(GL_COLOR_BUFFER_BIT)
-        glUseProgram(self.__shader)
         glActiveTexture(GL_TEXTURE0)
-        glBindTexture(GL_TEXTURE_2D, self.__texture_id)
-        glBindVertexArray(self.__rect_vao)
+        glBindVertexArray(self.__vao)
+        glBindTexture(GL_TEXTURE_2D, self.__gLayer.texture)
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
-
-        glUseProgram(self.__shader_text)
-        glActiveTexture(GL_TEXTURE0)
-        glBindTexture(GL_TEXTURE_2D, self.__font_texture_id)
-        glBindVertexArray(self.__rect_vao)
-        glUniform2f(glGetUniformLocation(self.__shader_text,'resolution'),self.width,self.height)
-        glUniform2i(glGetUniformLocation(self.__shader_text,'cell_size'),8,16)
+        #glBindTexture(GL_TEXTURE_2D, self.__tLayer.texture)
         #glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
-
         glBindVertexArray(0)
         glBindTexture(GL_TEXTURE_2D, 0)
         glUseProgram(0)
+
         pygame.display.flip()
 
 class Main:
