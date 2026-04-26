090f40dba8d9059f4805a2036c61d8b61a502c8e
frank-deng <13382084679@163.com>
2026-04-26 12:26:23 +0800

    MailCenter added

--- webserver/mailcenter/__init__.py
+++ webserver/mailcenter/__init__.py
@@ -1,7 +1,27 @@
+import hashlib
 from util import Logger
 from util import load_module
 
-class MailCenter(Logger):
-    def __init__(self,app,config):
-        app['mailCenter']=self
+class MailCenterInstance(Logger):
+    def __load_users(self,users):
+        self._users=users
+        self._user_login={}
+        for item in users:
+            if 'password' in item:
+                self._user_login[item['username']]=item
+
+    def __init__(self,config):
+        self.__load_users(config['mail']['users'])
+
+    def auth(self,username,password):
+        if not username or not password or username not in self._user_login:
+            return False
+        userdata=self._user_login[username]
+        return userdata['password']==hashlib.sha256(password.encode('iso8859-1',errors='ignore')).hexdigest()
+
+
+def MailCenter(app):
+    if 'mailCenter' not in app:
+        app['mailCenter']=MailCenterInstance(app['config'])
+    return app['mailCenter']
 
--- webserver/web/mail.py
+++ webserver/web/mail.py
@@ -1,8 +1,5 @@
 import asyncio
 import logging
-import os
-import json
-import hashlib
 from aiohttp.web import Request
 from aiohttp.web import Response
 from aiohttp_jinja2 import template
@@ -10,20 +7,6 @@ from datetime import datetime
 from util.fonttool import FontProcessor
 from mailcenter import MailCenter
 from . import WebServer
-
-
-def do_login(userdata_all,username,password):
-    if not username or not password:
-        return 1
-    if username not in userdata_all:
-        return 2
-    userdata=userdata_all[username]
-    if 'password' not in userdata:
-        return 2
-    password_hash=hashlib.sha256(password.encode('iso8859-1',errors='ignore')).hexdigest()
-    if userdata['password']!=password_hash:
-        return 2
-    return 0
 
 
 @WebServer.get('/mail/login.asp')
@@ -38,10 +21,9 @@ async def login(req:Request):
         form_data=await req.post()
         username=form_data.get('username','')
         password=form_data.get('password')
-        res=do_login(config['mail']['user'],username,password)
-        if res==1:
+        if not username or not password:
             fail_info='用户名和密码不能为空'
-        elif res==2:
+        elif not MailCenter(req.app).auth(username,password):
             fail_info='登录失败'
         else:
             fail_info='成功'
