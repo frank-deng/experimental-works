aafd2190d88b8b8824b060af8c9a578a8d448bd5
frank-deng <13382084679@163.com>
2026-08-10 12:54:54 +0800

    24stat.asm updated

--- 24stat.asm
+++ 24stat.asm
@@ -1,4 +1,23 @@
+.model tiny
+.8086
+assume cs:code,ds:code,es:code,ss:code
+code segment
 org 100h
+start:
+mov ax,-12345
+lea di,num_str
+call int162str
+
+mov ah,40h
+mov bx,1
+xor ch,ch
+mov cl,byte ptr [num_str]
+lea dx,num_str
+inc dx
+int 21h
+
+mov ax, 4C00h
+int 21h
 
 frac_oper:
 push bp
@@ -8,8 +27,6 @@ and bx,3
 shl bx,1
 mov dx,frac_oper_table[bx]
 jmp dx
-frac_oper_table:
-dw frac_add,frac_sub,frac_mul,frac_div
 frac_oper_end:
 pop dx
 pop bp
@@ -47,10 +64,59 @@ frac_sub:
 mov ax,[bp+4]
 imul word ptr[bp+10]
 mov bx,ax
+mov ax,[bp+6]
 imul word ptr[bp+8]
 sub bx,ax
 mov ax,[bp+6]
 imul word ptr[bp+10]
 xchg ax,bx
 jmp frac_oper_end
+
+int162str:
+push ax
+push bx
+push cx
+push dx
+push si
+push di
+mov si,di
+inc di
+mov bx,10
+xor cx,cx
+or ax,ax
+jns int162str_div_loop
+neg ax
+mov byte ptr es:[di],'-'
+inc di
+int162str_div_loop:
+xor dx,dx
+div bx
+push dx
+inc cx
+or ax,ax
+jnz int162str_div_loop
+int162str_store:
+pop dx
+add dl,'0'
+mov byte ptr es:[di],dl
+inc di
+loop int162str_store
+int162str_end:
+mov bx,di
+sub bx,si
+dec bx
+mov byte ptr es:[si],bl
+pop di
+pop si
+pop dx
+pop cx
+pop bx
+pop ax
+ret
+
+frac_oper_table dw frac_add,frac_sub,frac_mul,frac_div
+num_str db 0,0,0,0,0,0,0,0
+
+code ends
+end start
 
